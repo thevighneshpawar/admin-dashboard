7b87c1983f10f482dfd70c5391c3518e6364cc16
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-07-15 16:44:46 +0530

    Product filter css fix

--- src/pages/products/ProductFilter.tsx
+++ src/pages/products/ProductFilter.tsx
@@ -8,28 +8,27 @@ type productsFilterProps = {
 }
 
 const ProductFilter = ({ children }: productsFilterProps) => {
+
+
+
+
+
     return <Card>
-        <Row justify={'space-between'}>
+        <Row justify={'space-between'} align={'middle'}>
             <Col>
-                <Row gutter={10}>
-
+                <Row gutter={10} align={'middle'}>
                     <Col>
-                        <Form.Item name="q">
-
-                            <Input.Search
-                                placeholder='search'
-                            />
-
+                        <Form.Item name="q" style={{ marginBottom: 0 }}>
+                            <Input.Search placeholder='search' />
                         </Form.Item>
                     </Col>
 
                     <Col>
-                        <Form.Item name="category">
+                        <Form.Item name="category" style={{ marginBottom: 0 }}>
                             <Select
                                 style={{ width: 150, marginLeft: 10 }}
                                 allowClear={true}
                                 placeholder='Select Category'
-
                             >
                                 <Select.Option value='admin'>Admin</Select.Option>
                                 <Select.Option value='customer'>Customer</Select.Option>
@@ -39,12 +38,11 @@ const ProductFilter = ({ children }: productsFilterProps) => {
                     </Col>
 
                     <Col>
-                        <Form.Item name="tenant">
+                        <Form.Item name="tenant" style={{ marginBottom: 0 }}>
                             <Select
                                 style={{ width: 150, marginLeft: 10 }}
                                 allowClear={true}
                                 placeholder='Select Restaurant'
-
                             >
                                 <Select.Option value='admin'>Admin</Select.Option>
                                 <Select.Option value='customer'>Customer</Select.Option>
@@ -54,16 +52,15 @@ const ProductFilter = ({ children }: productsFilterProps) => {
                     </Col>
 
                     <Col>
-                        <Form.Item name="ispublish">
+                        <Form.Item name="ispublish" style={{ marginBottom: 0 }}>
                             <Space>
                                 <Switch defaultChecked />
-                                <Typography.Text >
+                                <Typography.Text>
                                     Show Only Published
                                 </Typography.Text>
                             </Space>
                         </Form.Item>
                     </Col>
-
                 </Row>
             </Col>
             <Col>
